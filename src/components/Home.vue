bd2e205e874862e3bc84bbfb125cf9bbe027930d
hantao <hanntao@163.com>
2018-01-18 14:05:44 +0800

    实现登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 主页 -->
+    <div>主页</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
