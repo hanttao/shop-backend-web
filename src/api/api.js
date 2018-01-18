8c61e9a4520bf13d64509ce65aa7c79cdaa3666f
hantao <hanntao@163.com>
2018-01-18 14:14:35 +0800

    设置请求的基准路径和修改调试配置

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-
+// 设置请求的基准路径
+axios.defaults.baseURL = 'http://47.96.21.88:8888/api/private/v1/'
 // 实现登录功能
 export const login = (params) => {
-  return axios.post('http://47.96.21.88:8888/api/private/v1/login', params).then(res => {
+  return axios.post('login', params).then(res => {
     // 返回具体的数据
     return res.data
   })
